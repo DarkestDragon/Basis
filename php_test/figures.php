048920bf7bc9dbc4fb08debb5bdf4cb605105abe
DarkestDragon <53903518+DarkestDragon@users.noreply.github.com>
2019-08-19 13:53:28 +0300

    Add files via upload

--- php_test/figures.php
+++ php_test/figures.php
@@ -3,19 +3,31 @@
 	require_once "./classes.php";
 	require_once "./figures_funcs.php";
 	
-	echo "Тестовое задание 3\n";
-	function task3(int $array_size){
-		for($i = 0; $i < $array_size; $i++){
-			$fig_array[] = rand_figure();
-			$area_array[] = $fig_array[$i]->get_area();
+	function sort_figs(array $figs, array $areas) : array{
+		$new_array = [];
+		$max_index = count($figs);
+		for($i = 0; $i < $max_index; $i++){
+			for($i2 = 0; $i2 < $max_index; $i2++){
+				if($figs[$i2]->get_area() == $areas[$i]){
+					$new_array[$i] = $figs[$i2];
+					break;
+				}
+			}
 		}
-			save_file("./figures.txt", $fig_array);
-			unset($fig_array);
-			$fig_array = load_file("./figures.txt");
-			print_r($fig_array);
+		return $new_array;
 	}
 	
-	task3(10);
+	echo "Тестовое задание 3\n";
+
+	for($i = 0; $i < 10; $i++){
+		$fig_array[] = rand_figure(0, 128);
+		$area_array[] = $fig_array[$i]->get_area();
+	}
+	save_file("./figures.txt", $fig_array);
+	unset($fig_array);
+	$fig_array = load_file("./figures.txt");
 	rsort($area_array);
-	print_r($area_array);
+	print_r($fig_array);
+	$new_fig_array = sort_figs($fig_array, $area_array);
+	print_r($new_fig_array);
 
