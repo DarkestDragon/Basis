181184f7e002429558b262ec4e036f777ac14c88
DarkestDragon <53903518+DarkestDragon@users.noreply.github.com>
2019-08-26 09:27:50 +0300

    Add files via upload

--- php_test/writers.sql
+++ php_test/writers.sql
@@ -17,14 +17,32 @@ CREATE TABLE book_authors( --links writers and books table
 -- load values
 INSERT INTO writers (writer_id, author) VALUES
 	(0, "Пехов"),
-	(1, "Ложкин"),
-	(2, "Киплинг"),
-	(3, "Данте"),
-	(4, "Пирс");
+	(1, "Достоевский"),
+	(2, "Лавкрафт"),
+	(3, "Бычкова");
 INSERT INTO books (book_id, book_name) VALUES
-	;
+	(0, "Крадущийся в тени"),
+	(1, "Джанга с тенями"),
+	(2, "Вьюга теней"),
+	(3, "Преступление и наказание"),
+	(4, "Заклинатели"),
+	(5, "Ловушка для духа"),
+	(6, "Зов Ктулху"),
+	(7, "Ужас Данвича")
+	(8, "Ловцы удачи"),
+	(9, "Особый почтовый)";
 INSERT INTO book_authors (link_id, writer_id, book_id) VALUES
-	;
+	(0, 0, 0),
+	(1, 0, 1),
+	(2, 0, 2),
+	(3, 0, 4),
+	(4, 3, 4),
+	(5, 0, 5),
+	(6, 3, 5)
+	(7, 2, 6),
+	(8, 2, 7),
+	(9, 0, 8),
+	(10, 0, 9);
 --
 
 --selecting authors, who has written no more than 6 books
