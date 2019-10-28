1ca9f33062889aba02e3e16f387da1904bd27807
DarkestDragon <53903518+DarkestDragon@users.noreply.github.com>
2019-10-28 11:52:05 +0300

    Update fibonacci.php

--- php_test/fibonacci.php
+++ php_test/fibonacci.php
@@ -2,8 +2,8 @@
 	#include_once 'classes.php';
 	
 	echo "Тестовое задание 1\n";
-	$lval = 1;
-	$rval = 2;
+	$lval = 0;
+	$rval = 1;
 	echo "0 -> 1\n1 -> 2\n";
 	for($i = 3; $i < 65; $i++){
 		$res = $lval + $rval;
